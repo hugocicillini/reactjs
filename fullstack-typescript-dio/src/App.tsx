cf07140d9fcbba5867fedcd33572d035b5847c67
Hugo Cicillini <hugo.cicillini@hotmail.com>
2024-01-07 22:09:10 -0300

    .

---
+++ fullstack-typescript-dio/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './assets/components/Header/Header'
+import Card from './assets/components/Card'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card text={"Faça Login!"} />
+    </>
+  )
+}
+
+export default App
